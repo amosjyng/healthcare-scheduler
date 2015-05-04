736fac75d934c5cd8d1561d931a59d10cfc35edc
Amos Ng <amosjyng@gmail.com>
2015-05-04 04:22:25 -0400

    Add generation of patient preferences

--- generate_patients.py
+++ generate_patients.py
@@ -1,44 +1,44 @@
 from pymc import *
 import numpy as np
-from math import sqrt
+from math import sqrt, exp, fabs
 
 wealth_modifier = [0.2, 1.0, 1.5]
 
 N_PATIENTS = 100
 
 
-def remembered_rand(gender, age, wealth, appt_time):
+def will_go_rand(gender, age, wealth, preferred, appt_time):
 	base_p = 0.8
 	base_p *= wealth_modifier[wealth]
-	base_p *= (appt_time / 60 - 8 + 2) / 10.0
+        base_p *= 1.5 / (fabs(preferred - appt_time) / 60.0)
 	base_p *= (100 - age + 20) / 100.0
 	base_p = pow(base_p, 1 / 10.0)
-	return Bernoulli('r', p=base_p)
+	return Bernoulli('w', p=base_p)
 
-def remembered_logp(value, gender, age, wealth, appt_time):
+def will_go_logp(value, gender, age, wealth, preferred, appt_time):
 	base_p = 0.5
 	base_p *= wealth_modifier[wealth]
-	base_p *= (appt_time / 60 - 8 + 2) / 10.0
+        base_p *= 1.5 / (fabs(preferred - appt_time) / 60.0)
 	base_p *= (100 - age + 20) / 100.0
 	base_p = pow(base_p, 1 / 10.0)
 	return bernoulli_like(value, base_p)
 
-def on_time_rand(gender, age, wealth, appt_time):
+def on_time_rand(gender, age, wealth, preferred, appt_time):
 	base_p = 0.5
 	base_p *= wealth_modifier[wealth]
-	base_p *= (appt_time / 60 - 8 + 2) / 10.0
+        base_p *= 1.5 / (fabs(preferred - appt_time) / 60.0)
 	base_p *= (100 - age + 20) / 100.0
 	return Bernoulli('o', p=base_p)
 
-def on_time_logp(value, gender, age, wealth, appt_time):
+def on_time_logp(value, gender, age, wealth, preferred, appt_time):
 	base_p = 0.5
 	base_p *= wealth_modifier[wealth]
-	base_p *= (appt_time / 60 - 8 + 2) / 10.0
+        base_p *= 1.5 / (fabs(preferred - appt_time) / 60.0)
 	base_p *= (100 - age + 30) / 100.0
 	return bernoulli_like(value, base_p)
 
-def tardiness_rand(remembered, on_time):
-	if remembered:
+def tardiness_rand(will_go, on_time):
+	if will_go:
 		if on_time:
 			return 0
 		else:
@@ -46,8 +46,8 @@ def tardiness_rand(remembered, on_time):
 	else:
 		return 30
 
-def tardiness_logp(value, remembered, on_time):
-	if remembered:
+def tardiness_logp(value, will_go, on_time):
+	if will_go:
 		if on_time:
 			return 0 if value == 0 else -np.inf
 		else:
@@ -56,21 +56,22 @@ def tardiness_logp(value, remembered, on_time):
 		return 0 if value == 30 else -np.inf
 
 def tardiness(patient, appt_time):
-	remembered = Stochastic(name='remembered', doc='Did the patient remember?', parents = {'gender': patient[1], 'age' : patient[2], 'wealth': patient[3], 'appt_time': appt_time}, random=remembered_rand, logp=remembered_logp, dtype=int)
-	on_time = Stochastic(name='on_time', doc='Did the patient come on time?', parents = {'gender': patient[1], 'age' : patient[2], 'wealth': patient[3], 'appt_time': appt_time}, random=on_time_rand, logp=on_time_logp, dtype=int)
-	return Stochastic(name='tardiness', doc='How late was the patient?', parents = {'remembered': remembered, 'on_time': on_time}, random=tardiness_rand, logp=tardiness_logp, dtype=int)
+	will_go = Stochastic(name='will_go', doc='Does the patient decide to go?', parents = {'gender': patient[1], 'age' : patient[2], 'wealth': patient[3], 'preferred': patient[4], 'appt_time': appt_time}, random=will_go_rand, logp=will_go_logp, dtype=int)
+	on_time = Stochastic(name='on_time', doc='Does the patient come on time?', parents = {'gender': patient[1], 'age' : patient[2], 'wealth': patient[3], 'preferred': patient[4], 'appt_time': appt_time}, random=on_time_rand, logp=on_time_logp, dtype=int)
+	return Stochastic(name='tardiness', doc='How late was the patient?', parents = {'will_go': will_go, 'on_time': on_time}, random=tardiness_rand, logp=tardiness_logp, dtype=int)
 
-patients = np.ndarray(shape=(N_PATIENTS, 4), dtype=object)
+patients = np.ndarray(shape=(N_PATIENTS, 5), dtype=object)
 
 for i in range(N_PATIENTS):
 	patients[i][0] = i
 	patients[i][1] = Bernoulli('gender_%i' % i, p=0.5)
 	patients[i][2] = Normal('age_%i' % i, mu=40, tau=0.01)
 	patients[i][3] = DiscreteUniform('wealth_%i' % i, lower=0, upper=2)
+        patients[i][4] = DiscreteUniform('preferred_%i' % i, lower=8*60, upper=18*60-20)
 
 if __name__ == '__main__':
 	appointments = np.ndarray(shape=(N_PATIENTS, 2), dtype=object)
 	for i in range(N_PATIENTS):
-		appointments[i][0] = DiscreteUniform('appt_%i' % i, lower=8*60, upper=18*60)
+		appointments[i][0] = DiscreteUniform('appt_%i' % i, lower=8*60, upper=18*60-20)
 		appointments[i][1] = tardiness(patients[i], appointments[i][0])
 	numpy.savetxt('patients.csv', np.concatenate((patients[:,1:4], appointments), axis=1), fmt='%i', delimiter=',', header='gender,age,wealth,appointment time,tardiness')
